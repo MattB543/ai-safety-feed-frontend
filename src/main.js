b33416b7d54266cefb302d87de55a5e85f8e1785
Matt Brooks <matt@incognitoforslack.com>
2025-04-25 23:19:32 -0400

    Adding PostHog

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import "./assets/main.css";
 import App from "./App.vue";
+import posthogPlugin from "./plugins/posthog";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(posthogPlugin);
+app.mount("#app");
